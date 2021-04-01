e25368c5826636a860dbdfa65528572f19756437
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-04-02 02:27:50 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string name = "Iftikhar Shams Niloy";
+    int age = 20;
+    float gpa = 3.85;
+    double pie = 3.1415926;
+    bool is_male = true;
+    cout << grade << endl;
+    cout << name << endl;
+    cout << age << endl;
+    cout << gpa << endl;
+    cout << pie << endl;
+    cout << is_male << endl;
+
+    return 0;
+}
