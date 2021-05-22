c26f89af048cb6e885cc2e54ddd905ae1a2d23ad
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-05-22 10:51:02 -0700

    Add files via upload

---
+++ Drawing Pattern 2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, something , nothing;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    int i,j;
+    for (i=0; i<n ; i++){
+        for(j=0 ; j<i ; j++){
+            printf("(*-*)");
+        }
+        printf("\n");
+    }
+    for (i=n ; i>0; i--) {
+            for(j=i ; j>0 ; j--){
+                printf("(*-*)");
+            }
+            printf("\n");
+    }
+    return 0;
+}
